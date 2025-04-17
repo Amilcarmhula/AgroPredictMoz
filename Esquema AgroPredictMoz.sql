329a4f0b557219e3bdd4d355d6f91b907a072dea
Amilcar Mula <amilcarmhula.am@gmail.com>
2025-04-17 20:32:55 +0200

    Ajuste da classh Previsao e do esquema de banco de dados

--- Esquema AgroPredictMoz.sql
+++ Esquema AgroPredictMoz.sql
@@ -1,14 +1,21 @@
+   drop database agropredictmoz;
 create database agropredictmoz;
 
 use agropredictmoz;
 
-create table previsoes(
+create table previsao(
 	id integer primary key auto_increment,
     localizacao varchar(100) not null,
     temperatura decimal(5,3) not null,
-    chuva decimal(5,3) not null,
-    recomendacoes varchar(300) not null,
+    umidade  decimal(5,3) not null,
+    precipitacao decimal(5,3) not null,
+    luz_solar integer not null,
     data_registo timestamp default current_timestamp
 );
 
-desc previsoes;
+create table recomendacao(
+	id integer primary key auto_increment,
+    recomendacoes varchar(300) not null,
+    fk_id_previsao integer not null,
+    foreign key (fk_id_previsao) references previsao(id)
+);
